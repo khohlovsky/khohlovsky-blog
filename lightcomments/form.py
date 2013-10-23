7a6c242d79a1921d31560fee9df15ec7b58e3e82
Khohlovsky <lazychyvak@gmail.com>
2013-10-23 14:37:28 +0400

    comments

--- lightcomments/form.py
+++ lightcomments/form.py
@@ -2,6 +2,8 @@
 from django import forms
     
 class CommentForm(forms.Form):
-    nick=forms.CharField(maxlentght=20)
-    capcha=forms.CharField('capcha',label=u'Капча',required='True')
-    message=forms.CharField('message',label=u'Комментарий', widget=forms.Textarea(attrs={'rows':9, 'cols':60,'wrap':'soft'}),required='True')
+    nick=forms.CharField(max_length=20,label="nick",widget=forms.TextInput(attrs={'placeholder':'nickname'}))
+    capcha=forms.CharField('capcha',label='capcha',required='True')
+    message=forms.CharField('message',label='comment',
+    widget=forms.Textarea(attrs={'rows':9,
+    'cols':60,'wrap':'soft','placeholder':'add your message'}),required='True')
